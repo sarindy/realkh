00186d3fec48db355f9003945eed7c7fbc4a28a4
Sarindy <sarindyo@yahoo.com>
2017-09-25 10:49:11 +0700

    123

--- src/main/java/com/real/contact/Phone.java
+++ src/main/java/com/real/contact/Phone.java
@@ -3,15 +3,12 @@ package com.real.contact;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.springframework.data.annotation.Persistent;
 
 @Entity
 @Table(name = "phone")
